1a4ee970b3e963c3b4c7537cb2d77196681246f5
longislandicetea <haoluo3@illinois.edu>
2015-03-06 13:38:18 -0600

    seperate coordinator and other nodes

--- configure.py
+++ configure.py
@@ -1,9 +1,18 @@
-PortList = [4001,4002,4003,4004]
+CoordinatorPort = 4005
+NodePortList = [4001, 4002, 4003, 4004]
 
-DelayList = [[0,5,6,7],[5,0,6,7],[6,6,0,5],[7,7,5,0]]
+CoordinatorDelay = 5
+NodeDelayList = [4, 5, 6, 7]
+#DelayList = [[0,5,6,7,8],[5,0,6,7,8],[6,6,0,5,8],[7,7,5,0,8],[5,6,7,8,0]]
 
-def GetPortNumber(chr):
-	return PortList[ord(chr[0])-ord('A')]
+def GetCoodPortNumber():
+	return CoordinatorPort
 
-def GetDelay(x,y):
-	return DelayList[x][y]
+def GetNodePortNumber(node_name):
+	return NodePortList[ord(node_name[0])-ord('A')]
+
+def GetCoodDelay():
+	return CoordinatorDelay
+
+def GetNodeDelay(node_name):
+	return NodeDelayList[ord(node_name[0]) - ord('A')]
